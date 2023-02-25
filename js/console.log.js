cca51a942b083011d24a2adbfaa860413be04ea1
owell andry <81540116+owellandry@users.noreply.github.com>
2023-02-25 06:36:17 +0000

    Updates

---
+++ js/console.log.js
@@ -0,0 +1,2 @@
+console.info("%c¡NO DEBERIAS ESTAR EN ESTE LUGAR!", "color:#fff; padding: 5px 10px; font-size:25px;");
+console.warn("%c¡NO DEBERIAS ESTAR EN ESTE LUGAR!", "color:#fff; padding: 5px 10px; font-size:25px;");
