995df03b9944ac8c987ebb6658c44c63fcfb4f88
owell andry <81540116+owellandry@users.noreply.github.com>
2023-02-26 22:27:42 +0000

    Updates

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,21 +1,50 @@
-document.addEventListener("keydown", function(event) {
-    // Verificar si la tecla presionada es "Print Screen"
-    if (event.key === "PrintScreen" || event.key === "PrintScreen" || event.keyCode === 44) {
-      // Evitar la acción por defecto de la tecla
-      event.preventDefault();
-      // Mostrar un mensaje de advertencia
-      alert("La captura de pantalla está deshabilitada en esta página.");
-    }
-  });
-  
+function myAlert(icon_alert, title_alert, text_alert) {
+    Swal.fire({
+        icon: icon_alert,
+        title: title_alert,
+        text: text_alert,
+        backdrop: true,
+        allowOutsideClick: false,
+        allowEscapeKey: false
+    });
+}
 
-  function enterFullScreen() {
-    var el = document.documentElement;
-    var rfs = el.requestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
-    if (typeof rfs != "undefined" && rfs) {
-      rfs.call(el);
+/** PERSONALIZACION DE CONSOLA ANTE ACCESO DE HERRAMIENTAS PARA DESARROLLADOR (F12) **/
+console.error('%cStop!', 'color: red;font-size:2.5rem;font-weight:bold;');  
+console.warn('%cEsta es una función del navegador destinada a desarrolladores. Si alguien le dijo que copie y pegue algo aquí para habilitar una función o "piratear" la integridad del sitio, será reportado y se le bloquerá el acceso a su cuenta.', 'font-size:1.25rem;line-height:1.1;'); 
+
+/** PARA DESHABILITAR CLIC DERECHO **/
+document.oncontextmenu = function() {
+    myAlert('warning', 'Clic derecho deshabilitado!', 'Trabajamos en mejorar el nivel de seguridad de nuestro sitio.');
+    return false;
+};
+
+/** PARA DESHABILITAR CAPTURA DE PANTALLA **/
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'PrintScreen') {
+        navigator.clipboard.writeText('');
+        myAlert('error', 'Capturas de pantalla deshabilitadas!', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
     }
-  }
-  
-  document.addEventListener("touchstart", enterFullScreen, false);
-  
+});
+
+/** PARA DESHABILITAR IMPRESIONES o EXPORTAR A PDF CON EL COMANDO (CTRL+P) Y DESHABILITAR RECORTES DE PANTALLA (CTRL+SHIFT+S)**/
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key == 'p') {
+        myAlert('error', 'Esta sección no se permite imprimir o exportar en PDF', 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado');
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    } else if (e.metaKey && e.shiftKey) {
+        Swal.fire({
+            imageUrl: 'https://www.soy502.com/sites/default/files/styles/full_node/public/2018/Mar/08/screen_shot_2018-03-08_at_7.31.17_am.png',
+            imageAlt: 'imagen de así te queria agarrar puerco',
+            imageWidth: '50%',
+            title: 'Recortes de pantalla detectados!',
+            text: 'Solicitamos no intentarlo de nuevo o su acceso será interrumpido y reportado',
+            backdrop: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            width: '90%'
+        });
+    }
+});
