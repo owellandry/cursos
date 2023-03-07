2e8a3be06f80adba0f8f49d1d673ab2f5ba43e6e
owell andry <81540116+owellandry@users.noreply.github.com>
2023-03-07 15:46:57 +0000

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,29 @@ var redireccion = "/app/anonyfeelcourses.apk";
 	  var tiempoActual = new Date().getTime();
 	  var url = redireccion + "?t=" + tiempoActual;
 	  qrcode.makeCode(url);
-	}, 10000);
+	}, 7000);
 	
 	qrcode._el.onclick = function() {
 	  window.location.href = redireccion;
 	};
 	
+
+	// Definimos la contraseña deseada
+const password = 'miContraseña';
+
+// Función para solicitar la contraseña al usuario
+function solicitarContraseña() {
+  const contraseñaIngresada = prompt("Ingrese la contraseña para acceder:");
+
+  if (contraseñaIngresada === password) {
+    // Si la contraseña ingresada es correcta, permitimos el acceso
+    alert("Contraseña correcta. Bienvenido.");
+  } else {
+    // Si la contraseña ingresada es incorrecta, volvemos a solicitarla
+    alert("Contraseña incorrecta. Intente nuevamente.");
+    solicitarContraseña();
+  }
+}
+
+// Llamamos a la función al cargar la página
+window.onload = solicitarContraseña;
